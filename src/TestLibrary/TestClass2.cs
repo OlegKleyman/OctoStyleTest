b6401353f7b3256145b440f35fc46ec8c196e8ca
Oleg Kleyman <Omego2K@gmail.com>
2015-06-28 10:51:19 -0400

    added second test class

---
+++ src/TestLibrary/TestClass2.cs
@@ -0,0 +1,17 @@
+﻿namespace TestLibrary
+{
+    public class TestClass2
+    {
+        public void testMethod()
+        {
+            foreach (var i in new [] {1,2,3,4})
+            {
+                
+            }
+            foreach (var b in new[] { 1, 2, 3, 4 })
+            {
+
+            }
+        }
+    }
+}

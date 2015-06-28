32da2029c146d4a529a356ab8a349b1593a93dc9
Oleg Kleyman <Omego2K@gmail.com>
2015-06-28 10:47:34 -0400

    added first test class

---
+++ src/TestLibrary/TestClass.cs
@@ -0,0 +1,21 @@
+﻿//This is a test class used for analyzer tests. It's designed to be fail standards and is not for use
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLibrary
+{
+    public class TestClass
+    {
+        public void TestMethod()
+        {
+            if (1.ToString() == "1")
+            {
+
+            }
+            2.ToString();
+        }
+    }
+}
